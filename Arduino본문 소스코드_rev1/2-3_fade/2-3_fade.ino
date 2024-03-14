ef6a64000e1ee2b6394ab481ac408ff9d9c71da1
정가현 <135098077+gahyun01@users.noreply.github.com>
2024-03-14 23:38:53 +0900

    Add files via upload

---
+++ Arduino본문 소스코드_rev1/2-3_fade/2-3_fade.ino
@@ -0,0 +1,22 @@
+const int LED = 9;      // LED 핀을 9번 핀으로 정의
+void setup()
+{
+  pinMode(LED, OUTPUT);     // LED 핀을 출력으로 설정
+}
+
+void loop()
+{
+  for (int i = 0; i < 256; i++)
+  {
+    analogWrite(LED, i);
+    delay(10);
+  }
+  analogWrite(LED, 0);
+  delay(5000);
+  for (int i = 255; i >= 0; i--)
+  {
+    analogWrite(LED, i);
+    delay(10);
+  }
+  delay(5000);
+}
